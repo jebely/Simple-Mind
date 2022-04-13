4084dffd7c56e331ea9b40310c5e47b6371c49e4
Jeb <jebely.88@gmail.com>
2022-04-12 21:38:22 -0400

    Initial Commit

---
+++ Simple Mind/Simple_MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_MindApp.swift
+//  Simple Mind
+//
+//  Created by Jeb Ely on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
